7335a109fe532c0004a9e601aee70a3a77605e20
Tori Daughton <toriddaughton@gmail.com>
2022-05-30 17:35:45 -0600

    checking module, unsure of current answers

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+PATH = "db/jobs.sqlite"
 
 app = Flask(__name__)
+
+def open_connection():
+    connection = getattr(g, '_connection', None)
+    if connection == None:
+        (connection, g._connection) = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+    return connection
 
 @app.route("/")
 @app.route("/jobs")
