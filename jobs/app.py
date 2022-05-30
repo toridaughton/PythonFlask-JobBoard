6e43aa9773385e73f038359cc1164815ad398824
Tori Daughton <toriddaughton@gmail.com>
2022-05-30 17:50:55 -0600

    checking module update

--- jobs/app.py
+++ jobs/app.py
@@ -23,7 +23,7 @@ def execute_sql(sql, values=(), commit=False, single=False):
     
     cursor.close()
     return results
-@app.teardown_appconntext
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
