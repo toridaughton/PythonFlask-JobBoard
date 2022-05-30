6aafe1f8d270ef9acff0e735748f8fb18f69434b
Tori Daughton <toriddaughton@gmail.com>
2022-05-30 17:50:09 -0600

    checking module completion

--- jobs/app.py
+++ jobs/app.py
@@ -1,3 +1,4 @@
+from distutils.util import execute
 from flask import Flask, render_template, g
 import sqlite3
 
@@ -8,9 +9,25 @@ app = Flask(__name__)
 def open_connection():
     connection = getattr(g, '_connection', None)
     if connection == None:
-        (connection, g._connection) = sqlite3.connect(PATH)
+        connection = g._connection = sqlite3.connect(PATH)
     connection.row_factory = sqlite3.Row
     return connection
+
+def execute_sql(sql, values=(), commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if single else cursor.fetchall()
+    
+    cursor.close()
+    return results
+@app.teardown_appconntext
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
 
 @app.route("/")
 @app.route("/jobs")
