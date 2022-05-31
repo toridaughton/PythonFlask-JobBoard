ea3b7cf4ddc645e8c79c9b8a47fd8681d1036b32
Tori Daughton <toriddaughton@gmail.com>
2022-05-31 09:19:52 -0600

    checking module completion

--- jobs/app.py
+++ jobs/app.py
@@ -49,4 +49,4 @@ def employer(employer_id):
 
     reviews = execute_sql('SELECT review, rating, title, date, status FROM review JOIN employer ON employer.id = review.employer_id WHERE employer.id = ?', [employer_id])
 
-    return render_template("employer.html", employer=employer, jobs=jobs)
+    return render_template("employer.html", employer=employer, jobs=jobs, reviews=reviews)
