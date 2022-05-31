785e2b380a2f092679bf7545aaa1e0c4da5ec651
Tori Daughton <toriddaughton@gmail.com>
2022-05-31 15:44:29 -0600

    checking module update

--- jobs/app.py
+++ jobs/app.py
@@ -61,5 +61,5 @@ def review(employer_id):
         status = request.form['status']
         date = datetime.datetime.now().strftime("%m/%d/%Y")
         execute_sql('INSERT INTO review(review, rating, title, date, status, employer_id) VALUES (?, ?, ?, ?, ?, ?)', (review, rating, title, date, status, employer_id), commit=True)
-        redirect(url_for('employer.html'))
+        return redirect(url_for('employer.html'))
     return render_template("review.html", employer_id=employer_id)
